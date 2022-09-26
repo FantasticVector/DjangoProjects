5e210b94c2e86f9c8169bb2e5f8312a9ea30aeab
FantasticVector <FantasticVector@gmail.com>
2022-09-26 21:09:58 +0530

    connected my databases to mysql

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zfau8o3!#6p^3v0f@*=it7%#&!-*3f=ns)0@+c)(g-#^@pfy+v'
+SECRET_KEY = 'hello-world'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'nine'
     }
 }
 
